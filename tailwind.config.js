3fc4ddbc637553f9bf0626e5e4a07c3eeecca496
Adophilus <uchenna19of@gmail.com>
2022-04-16 13:42:01 +0100

    added a portfolio project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // Bookmarker
         "bookmark-purple": "#5267DF",
         "bookmark-red": "#FA5959",
         "bookmark-blue": "#242A45",
         "bookmark-grey": "#9194A2",
-        "bookmark-white": "#f7f7f7"
+        "bookmark-white": "#f7f7f7",
+
+        // Portfolio
+        "body": "#17171F",
+        "selected-text": "#A3A3FF",
+        "theme": "#3F3FFF",
+        "nav": "#404053",
+        "secondary": "#9191A4",
+        "badge": "#3F3F51",
+        "input-border": "#565666",
+        "input": "#2A2A35"
       }
     },
     fontFamily: {
